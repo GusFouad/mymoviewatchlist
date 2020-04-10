951edeeb225a93abc795192dc5bc865bf3e62d40
Gus <gus.fouad@gmail.com>
2020-04-10 15:01:44 -0400

    feat: complete navbar

---
+++ client/src/components/registerform.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class RegisterForm extends Component {
+  state = {};
+  render() {
+    return <h1>Register form</h1>;
+  }
+}
+
+export default RegisterForm;
